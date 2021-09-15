82d34263adbb80a79a96fadc21ca617e363587c5
DevinDaniel <1041379793@qq.com>
2021-09-15 12:13:27 +0800

    second commit

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"category/common"
 	"category/domain/repository"
+	service2 "category/domain/service"
 	"category/handler"
+	category "category/proto/category"
 	"github.com/asim/go-micro/plugins/registry/consul/v3"
 	"github.com/asim/go-micro/v3"
-	category "category/proto/category"
 	log "github.com/asim/go-micro/v3/logger"
 	"github.com/asim/go-micro/v3/registry"
 	"github.com/jinzhu/gorm"
-	service2 "category/domain/service"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
@@ -39,15 +40,16 @@ func main() {
 	mysqlInfo := common.GetMysqlFromConsul(consulConfig,"mysql")
 	//链接数据库
 	db,err := gorm.Open("mysql",mysqlInfo.User+":"+mysqlInfo.Pwd+"@/"+mysqlInfo.Database+"?charset=utf8&parseTime=True&loc=Local")
-	if err!=nil{
+	if err !=nil {
 		log.Error(err)
 	}
 	defer db.Close()
 	//禁止复表
 	db.SingularTable(true)
+
 	//创建表 只执行一次
-	rp := repository.NewCategoryRepository(db)
-	rp.InitTable()
+	//rp := repository.NewCategoryRepository(db)
+	//rp.InitTable()
 
 	//Initialise service
 	service.Init()
